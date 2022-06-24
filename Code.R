280f504b610e3d130600c4cc0700b4429e217196
sashadata <63408623+sashadata@users.noreply.github.com>
2022-06-24 15:38:22 -0400

    update

--- Code.R
+++ Code.R
@@ -1,1 +1,52 @@
-##### writing
+#################### Project 6: Regression analysis
+#### Predict number of cases a client likely to open based on collected demographics
+#### Apply multiple regression to examine how multiple predictor variables can be used to model a response variable
+####################
+
+### Load and explore the dataset
+
+data<-read.csv("demo.csv")
+
+str(data)
+
+### The following data was collected on 1187 clients.
+### This is a very rich dataset with the following columns: 
+### Client _id - id of client assigned at the stage of enrollment 
+### Children - number of children in the household
+### Income - income of client
+### NofCases - number of cases opened by clients during program
+### Age - age of client
+### Score - assessment of survey completed by client
+### Days - total number of days being enrolled in the program
+### Services - total number of services received by clients
+
+
+#### All of the variables are quantitative
+#### On average client has opened 3.6 cases. The range is 7, min is 2, and max is 4. 
+
+library(tidyverse)
+library(dplyr)
+summary(data)
+
+### The first column is id. Not useful for analysis
+
+data<-subset(data, select = Children:Services) 
+
+
+### In the following pairs plot of the other 7 quantitative values, there is a slight    
+### relationship between number of cases and age, number of cases and services, and quite 
+### strong relationship between number of cases and number of children in the 
+### household
+pairs(data)
+
+### Run correlation analysis to tell us the relationship between variables.
+### Notice that most of the correlations are at or below 0.20. 
+### The highest is between nofcases and children 0.81
+### Then, age and nofcaaes is correlated at .21 
+### Services and nofcases is .22
+
+round(cor(data),2)
+
+#### Create a multiple linear regression model to number of cases from the rest of the
+#### explanatory variables
+
